49b6b1675c3a8b8b774091a06f202ce162fb49d6
Rodrigo Teixeira Silva <60649228+rodrigo-teixeira-silva@users.noreply.github.com>
2023-11-25 15:33:32 -0400

    Update HomeHeader.tsx

--- mobile/src/components/HomeHeader.tsx
+++ mobile/src/components/HomeHeader.tsx
@@ -1,46 +1,40 @@
 import { Heading, HStack, Icon, Text, VStack } from 'native-base';
-
 import {MaterialIcons} from '@expo/vector-icons'
-
 import { UserPhoto } from './UserPhoto';
 import { TouchableOpacity } from 'react-native';
 
+import defaulUserPhotoImg from '@assets/userPhotoDefault.png';
 import { useAuth } from '@hooks/useAuth';
-
-import defaulUserPhotoImg from '@assets/userPhotoDefault.png'; 
 
 export { Heading } from 'native-base';
 
 export function HomeHeader(){
-
   const { user, signOut } = useAuth();
 
   return(
     <HStack background= "gray.600" pt={16} pb={5} px={8} alignItems="center">
       <UserPhoto
-          source={user.avatar ? { uri: user.avatar } : defaulUserPhotoImg }
+         source={user.avatar ? { uri: user.avatar } : defaulUserPhotoImg}
           alt='Imagem do usuário'
           size={16}
           mr={4}
       />
-
       <VStack flex={1}>
         <Text color="gray.100" fontSize="md">
           Olá
         </Text>
-
         <Heading color="gray.100" fontSize="md" fontFamily="heading">
-         {user.name}
+          Rodrigo
         </Heading>
       </VStack>
 
         <TouchableOpacity onPress={signOut}>
-          <Icon 
-          as={MaterialIcons} 
-          name='logout'
-          color="gray.200"
-          size={7}
-          />
+        <Icon 
+        as={MaterialIcons} 
+        name='logout'
+        color="gray.200"
+        size={7}
+        />
         </TouchableOpacity>
         
      </HStack>
