a75517811ea91f72e7861d73a81840d46a17f9e5
Rodrigo Teixeira Silva <60649228+rodrigo-teixeira-silva@users.noreply.github.com>
2024-09-18 02:15:34 -0300

    feat: Searching exercise groups, selecting exercises by group and adding DTO exercises

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,6 +1,10 @@
-import { useState } from "react";
-import { useNavigation } from "@react-navigation/native";
-import { HStack, VStack, FlatList, Heading,Text } from "native-base";
+import { useState, useEffect, useCallback } from "react";
+import { useNavigation, useFocusEffect} from "@react-navigation/native";
+import { HStack, VStack, FlatList, Heading, Text, useToast } from "native-base";
+
+import { api } from "../service/api";
+import { AppError } from "@utils/AppError";
+import { ExerciseDTO } from "@dtos/exerciseDTO";
 
 import { appNavigatorRoutesProps } from "@routes/app.routes";
 
@@ -8,51 +12,110 @@ import { HomeHeader } from "@components/HomeHeader";
 import { Group } from "@components/Group";
 import { ExercisesCard } from "@components/ExercisesCard";
 
-export function Home(){
-    const [Groups, setGroups] = useState(["Costas","Biceps","Triceps","Ombro"])
-    const [groupSelected, setGroupSelected] = useState('Costas');
 
-    const navigation = useNavigation<appNavigatorRoutesProps>();
-    
-    function handleOpenExercicesDetails(){
-        navigation.navigate('exercises');
+export function Home() {
+  const [Groups, setGroups] = useState<string[]>([]);
+  const [exercises, setExercises] = useState<ExerciseDTO[]>([]);
+  const [groupSelected, setGroupSelected] = useState("Costas");
+
+  const toast = useToast();
+  const navigation = useNavigation<appNavigatorRoutesProps>();
+
+  function handleOpenExercicesDetails() {
+    navigation.navigate("exercises");
+  }
+
+  async function fetGroups() {
+    try {
+      const response = await api.get("/groups");
+      setGroups(response.data);
+    } catch (error) {
+      const isAppError = error instanceof AppError;
+      const title = isAppError
+        ? error.message
+        : "Não foi possível carregar os grupos musculares.";
+
+      toast.show({
+        title,
+        placement: "top",
+        bgColor: "red.500",
+      });
     }
+  }
 
-    return(
-        <VStack flex={1}>
-        <HomeHeader/>
+  async function fetchExercisesByGroups() {
+    try {
+
+        const response = await api.get(`/exercises/bygroup/${groupSelected}`);
+        console.log(response.data);
+
+    } catch (error) {
+      const isAppError = error instanceof AppError;
+      const title = isAppError
+        ? error.message
+        : "Não foi possível carregar os exercícios.";
+
+      toast.show({
+        title,
+        placement: "top",
+        bgColor: "red.500",
+      });
+    }
+  }
+
+  useEffect(() => {
+    fetGroups();
+  }, []);
+
+  useFocusEffect(useCallback(()=>{
+    fetchExercisesByGroups();
+  }, [groupSelected]))
+
+  return (
+    <VStack flex={1}>
+      <HomeHeader />
+
+      <FlatList
+        data={Groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <Group
+            name={item}
+            isActive={
+              groupSelected.toLocaleUpperCase() === item.toLocaleUpperCase()
+            }
+            onPress={() => setGroupSelected(item)}
+          />
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        _contentContainerStyle={{ px: 8 }}
+        my={10}
+        maxH={10}
+      />
+      <VStack flex={1} px={8}>
+        <HStack justifyContent="space-between" mb={5}>
+          <Heading color="gray.200" fontSize="md">
+            Exercicios
+          </Heading>
+
+          <Text color="gray.200" fontSize="sm">
+            4
+          </Text>
+        </HStack>
 
         <FlatList 
-            data={Groups}
-            keyExtractor={item => item}
-            renderItem={ ({item})=>(
-                <Group 
-                name={item}
-                isActive={groupSelected.toLocaleUpperCase() === item.toLocaleUpperCase()}
-                onPress={ () => setGroupSelected(item)}
-                />
-            )}
-            horizontal
-            showsHorizontalScrollIndicator={false}
-            _contentContainerStyle={{ px:8 }}
-            my={10}
-            maxH={10}
+          data={exercises}
+          keyExtractor={item => item.id}
+          renderItem={({ item }) => (
+        <ExercisesCard onPress={handleOpenExercicesDetails} />
+          )}
+          showsVerticalScrollIndicator={false}
+          _contentContainerStyle={{
+            paddingBottom: 20
+          }}
         />
-        <VStack flex={1} px={8}>
-            <HStack justifyContent="space-between" mb={5}>
-                <Heading color="gray.200" fontSize="md">
-                    Exercicios
-                </Heading>
-
-                <Text color="gray.200" fontSize="sm">
-                    4
-                </Text>
-            </HStack>
-            <ExercisesCard
-                onPress={handleOpenExercicesDetails}
-            />
-
-        </VStack>
+      </VStack>
     </VStack>
-    );
+  );
 }
